eaf12ab81c4e86b0c61ce403ffe13c303658f643
Pedro Militao <pedromilitao@denox.com.br>
2021-07-29 16:49:07 -0300

    Audio database removed

--- lib/src/model/audio_database.dart
+++ lib/src/model/audio_database.dart
@@ -1,29 +1,29 @@
-import 'dart:math';
+// import 'dart:math';
 
-import 'package:flutter_smart_course/src/model/hive/hive_models.dart';
-import 'package:hive/hive.dart';
+// import 'package:flutter_smart_course/src/model/hive/hive_models.dart';
+// import 'package:hive/hive.dart';
 
-class AudioDatabase {
-  static const _boxName = 'audios2';
+// class AudioDatabase {
+//   static const _boxName = 'audios2';
 
-  Future addReading(HiveReading reading) async {
-    var box = await Hive.openBox<HiveReading>(_boxName);
-    box.put(reading.id, reading);
-  }
+//   Future addReading(HiveReading reading) async {
+//     var box = await Hive.openBox<HiveReading>(_boxName);
+//     box.put(reading.id, reading);
+//   }
 
-  Future deleteReading(HiveReading reading) async {
-    var box = await Hive.openBox<HiveReading>(_boxName);
-    if (reading != null) box.delete(reading.id);
-  }
+//   Future deleteReading(HiveReading reading) async {
+//     var box = await Hive.openBox<HiveReading>(_boxName);
+//     if (reading != null) box.delete(reading.id);
+//   }
 
-  Future<HiveReading> getReading(int id) async {
-    var box = await Hive.openBox<HiveReading>(_boxName);
-    return box.get(id);
-  }
+//   Future<HiveReading> getReading(int id) async {
+//     var box = await Hive.openBox<HiveReading>(_boxName);
+//     return box.get(id);
+//   }
 
-  Future<List<HiveReading>> getReadingList() async {
-    var box = await Hive.openBox<HiveReading>(_boxName);
-    var list = box.values?.toList() ?? <HiveReading>[];
-    return list;
-  }
-}
+//   Future<List<HiveReading>> getReadingList() async {
+//     var box = await Hive.openBox<HiveReading>(_boxName);
+//     var list = box.values?.toList() ?? <HiveReading>[];
+//     return list;
+//   }
+// }
