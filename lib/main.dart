4d7bd97b5d8384baf3177ba091133b0ed37ac815
Pedro Militao <pedromilitao@denox.com.br>
2022-02-13 10:09:22 -0300

    Flutter update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_smart_course/src/model/audio_database.dart';
 import 'package:flutter_smart_course/src/model/hive/hive_models.dart';
 import 'package:flutter_smart_course/src/model/quizz_database.dart';
@@ -10,6 +11,7 @@ import 'package:path_provider/path_provider.dart' as pathProvider;
 import 'package:hive/hive.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:starflut/starflut.dart';
 import 'src/theme/theme.dart';
 
 Future<void> main() async {
@@ -28,13 +30,24 @@ Future<void> main() async {
   Hive.registerAdapter(AnswerAdapter());
   Hive.registerAdapter(ReadingErrorAdapter());
   Hive.registerAdapter(ErrorControllerAdapter());
-  
 
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _platformVersion = 'Unknown';
+  @override
+  void initState() {
+    super.initState();
+    runPythonCodePrep();
+  }
+
   @override
   Widget build(BuildContext context) {
     var colorScheme = ColorScheme.light(
@@ -118,4 +131,73 @@ class MyApp extends StatelessWidget {
         headline2: baseTheme.headline5.copyWith(fontSize: 60, color: color),
         headline1: baseTheme.headline5.copyWith(fontSize: 96, color: color),
       );
+
+  Future<void> runPythonCodePrep() async {
+    String platformVersion;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      StarCoreFactory starcore = await Starflut.getFactory();
+      StarServiceClass Service = await starcore.initSimple("test", "123", 0, 0, []);
+      await starcore
+          .regMsgCallBackP((int serviceGroupID, int uMsg, Object wParam, Object lParam) async {
+        print("$serviceGroupID  $uMsg   $wParam   $lParam");
+
+        return null;
+      });
+      StarSrvGroupClass SrvGroup = await Service["_ServiceGroup"];
+
+      /*---script python--*/
+      bool isAndroid = await Starflut.isAndroid();
+      if (isAndroid == true) {
+        await Starflut.copyFileFromAssets(
+            "testcallback.py", "flutter_assets/starfiles", "flutter_assets/starfiles");
+        await Starflut.copyFileFromAssets(
+            "testpy.py", "flutter_assets/starfiles", "flutter_assets/starfiles");
+        await Starflut.copyFileFromAssets(
+            "python3.6.zip", "flutter_assets/starfiles", null); //desRelatePath must be null
+        await Starflut.copyFileFromAssets("zlib.cpython-36m.so", null, null);
+        await Starflut.copyFileFromAssets("unicodedata.cpython-36m.so", null, null);
+        await Starflut.loadLibrary("libpython3.6m.so");
+      }
+
+      String docPath = await Starflut.getDocumentPath();
+      print("docPath = $docPath");
+      String resPath = await Starflut.getResourcePath();
+      print("resPath = $resPath");
+      dynamic rr1 = await SrvGroup.initRaw("python36", Service);
+
+      print("initRaw = $rr1");
+      var result = await SrvGroup.loadRawModule(
+          "python", "", resPath + "/flutter_assets/starfiles/" + "testpy.py", false);
+      print("loadRawModule = $result");
+      dynamic python = await Service.importRawContext("python", "","", false, "");
+      print("python = " + await python.getString());
+      StarObjectClass retobj = await python.call("tt", ["hello ", "world"]);
+      print(await retobj[0]);
+      print(await retobj[1]);
+      print(await python["g1"]);
+      StarObjectClass yy = await python.call("yy", ["hello ", "world", 123]);
+      print(await yy.call("__len__", []));
+      StarObjectClass multiply = await Service.importRawContext("python", "Multiply","", true, "");
+      StarObjectClass multiplyInst = await multiply.newObject(["", "", 33, 44]);
+      print(await multiplyInst.getString());
+      print(await multiplyInst.call("multiply", [11, 22]));
+      await SrvGroup.clearService();
+      await starcore.moduleExit();
+      platformVersion = 'Python 3.6';
+    } on PlatformException catch (e) {
+      print("{$e.message}");
+      platformVersion = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+
+    if (!mounted) return;
+
+    setState(() {
+      _platformVersion = platformVersion;
+    });
+  }
 }
